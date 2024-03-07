2dbd75d8e180dcd1710e2ec8e395f03115cf6e5f
Uladimir Hrydzin <hrydzin@gmail.com>
2024-03-07 06:59:00 +0400

    feat: add swaapper

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const port = process.env.PORT || 4001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
+  console.log('port:', port);
 }
 bootstrap();
